f4431ae9d91ad2bc1283fe8b0706676aa5d2dae2
Jan Bader <jan.bader@ub-software.de>
2017-06-04 03:54:35 +0200

    Implement Clipboard Manager

---
+++ Clip/Clipboard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using IDataObject = System.Windows.IDataObject;
+
+namespace Clip
+{
+    public class Clipboard
+    {
+        private static readonly IntPtr WndProcSuccess = IntPtr.Zero;
+
+        public Clipboard(Window windowSource)
+        {
+            var source = PresentationSource.FromVisual(windowSource) as HwndSource;
+            if (source == null)
+            {
+                throw new ArgumentException(
+                    "Window source MUST be initialized first, such as in the Window's OnSourceInitialized handler."
+                    , nameof(windowSource));
+            }
+
+            source.AddHook(WndProc);
+
+            // get window handle for interop
+            var windowHandle = new WindowInteropHelper(windowSource).Handle;
+
+            // register for clipboard events
+            NativeMethods.AddClipboardFormatListener(windowHandle);
+        }
+
+        public event EventHandler<ClipboardChangedEventArgs> ClipboardChanged;
+
+        private void OnClipboardChanged()
+        {
+            var obj = System.Windows.Clipboard.GetDataObject();
+            var entry = new ClipboardEntry(obj);
+            var args = new ClipboardChangedEventArgs(entry);
+            ClipboardChanged?.Invoke(this, args);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == NativeMethods.WM_CLIPBOARDUPDATE)
+            {
+                OnClipboardChanged();
+                handled = true;
+            }
+
+            return WndProcSuccess;
+        }
+    }
+
+    public class ClipboardChangedEventArgs : EventArgs
+    {
+        public readonly ClipboardEntry Data;
+
+        public ClipboardChangedEventArgs(ClipboardEntry dataObject)
+        {
+            Data = dataObject;
+        }
+    }
+
+    public class ClipboardEntry
+    {
+        private readonly IDataObject _dataObject;
+
+        public ClipboardEntry(IDataObject dataObject)
+        {
+            _dataObject = dataObject;
+        }
+
+        public string Text => GetData<string>(DataFormats.UnicodeText);
+
+        public T GetData<T>(string format)
+        {
+            if (!_dataObject.GetDataPresent(format))
+                return default(T);
+
+            return (T)_dataObject.GetData(format);
+        }
+    }
+}
