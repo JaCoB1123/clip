654e0f2036ec1c0ac6dbfa8aeaf46f4d8b62cf69
Jan Bader <jan.bader@ub-software.de>
2017-06-04 03:54:44 +0200

    Add simple frontend

---
+++ Frontend/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Documents;
+using Clip;
+using Clipboard = Clip.Clipboard;
+
+namespace Frontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private Clipboard _clipboard;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public BindingList<ClipboardEntry> Clips { get; } = new BindingList<ClipboardEntry>();
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Initialize the clipboard now that we have a window soruce to use
+            _clipboard = new Clipboard(this);
+            _clipboard.ClipboardChanged += ClipboardChanged;
+        }
+
+        private void ClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            Clips.Add(e.Data);
+        }
+    }
+}
